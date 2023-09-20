aeac0a64fdfc49d8f4f5ea85a04d15f953bcffed
Hamza-pi <mukhtarhamza294@gmail.com>
2023-09-20 22:23:26 +0500

    document error resolved

--- src/hooks/useOutsideAlerter.jsx
+++ src/hooks/useOutsideAlerter.jsx
@@ -1,29 +1,26 @@
 import { useEffect, useState } from "react";
 
-export default function useOutsideAlerter({menuRef, setMenuOpened}) {
- 
- useEffect(() => {
-  /**
-   * Alert if clicked on outside of element
-   */
+export default function useOutsideAlerter({ menuRef, setMenuOpened }) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    if (typeof document !== "undefined") {
+      const viewport_width = document.documentElement.clientWidth;
 
-  const viewport_width = document.documentElement.clientWidth;
-
-  function handleClickOutside(event) {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      if (viewport_width <= 640) {
-          setMenuOpened(false);
+      function handleClickOutside(event) {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+          if (viewport_width <= 640) {
+            setMenuOpened(false);
+          }
+        }
       }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
     }
-  }
-  // Bind the event listener
-  document.addEventListener("mousedown", handleClickOutside);
-  return () => {
-    // Unbind the event listener on clean up
-    document.removeEventListener("mousedown", handleClickOutside);
-  };
-}, [menuRef,setMenuOpened]);
-
-
-
+  }, [menuRef, setMenuOpened]);
 }
