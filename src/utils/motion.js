3dc9213771009fcfbd61b2b773c74ba4432b0c39
Hamza-pi <mukhtarhamza294@gmail.com>
2023-09-20 22:28:25 +0500

    doc not defined

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -191,7 +191,7 @@ export const listItem = {
 };
 
 export const getMenuStyles = (menuOpened) => {
-  if (document.documentElement.clientWidth <= 640) {
+  if (typeof document!=="undefined" && document.documentElement.clientWidth <= 640) {
     console.log("outside of sidebar reached")
     return { right: !menuOpened && "-100%" };
   }
