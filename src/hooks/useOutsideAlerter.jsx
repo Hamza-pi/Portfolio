4abccf6bba6af138fbefaf6290bef34c8261ed39
Hamza-pi <mukhtarhamza294@gmail.com>
2023-09-20 22:20:13 +0500

    document not found err resloved

--- src/hooks/useOutsideAlerter.jsx
+++ src/hooks/useOutsideAlerter.jsx
@@ -1,18 +1,13 @@
-'use client'
 import { useEffect, useState } from "react";
 
 export default function useOutsideAlerter({menuRef, setMenuOpened}) {
-
-  let viewport_width;
- if(typeof window){
-   viewport_width = window.document.documentElement.clientWidth;
-   
- }
  
  useEffect(() => {
   /**
    * Alert if clicked on outside of element
    */
+
+  const viewport_width = document.documentElement.clientWidth;
 
   function handleClickOutside(event) {
     if (menuRef.current && !menuRef.current.contains(event.target)) {
@@ -22,12 +17,12 @@ export default function useOutsideAlerter({menuRef, setMenuOpened}) {
     }
   }
   // Bind the event listener
-  window.document.addEventListener("mousedown", handleClickOutside);
+  document.addEventListener("mousedown", handleClickOutside);
   return () => {
     // Unbind the event listener on clean up
-    window.document.removeEventListener("mousedown", handleClickOutside);
+    document.removeEventListener("mousedown", handleClickOutside);
   };
-}, [menuRef,setMenuOpened,viewport_width]);
+}, [menuRef,setMenuOpened]);
 
 
 
